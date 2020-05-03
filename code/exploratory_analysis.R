b8c805c5341d63d01d05a4be99ae3a8704b746c6
Paraskevi Massara <p.massara@mail.utoronto.ca>
2020-05-03 00:02:11 -0400

    clinical variables exploration

--- code/exploratory_analysis.R
+++ code/exploratory_analysis.R
@@ -1,5 +1,6 @@
 
 
+
 ##===  Data Exploration ====
 
 ##=== ! SOS ! ======
@@ -11,12 +12,12 @@
 #TOC>
 #TOC>   Section  Title                                            Line
 #TOC> -----------------------------------------------------------------------------------------
-#TOC>   1        Packages                                          
-#TOC>   2        Missing data exploration                          
-#TOC>   3        Manipulate the dataset                            
+#TOC>   1        Packages
+#TOC>   2        Missing data exploration
+#TOC>   3        Manipulate the dataset
 #TOC>   4        Growth dataset
-#TOC>   5        Clinical dataset                                    
-#TOC>   6        Compute descriptive statistics                    
+#TOC>   5        Clinical dataset
+#TOC>   6        Compute descriptive statistics
 #TOC> =========================================================================================
 
 # ====    1  Packages  ========================================================================
@@ -79,17 +80,17 @@ plot(round(colSums(is.na(data)) / dim(data)[1], digits = 2) * 100)
 
 # Which columns have more than 50% missing data?
 
-high_rate <- missing_data[which(missing_data$prc_value >= 50),]
+high_rate <- missing_data[which(missing_data$prc_value >= 50), ]
 rownames(high_rate)
 
 # Which columns have more than 20% missing data?
 
-high_rate20 <- missing_data[which(missing_data$prc_value >= 20), ]
+high_rate20 <- missing_data[which(missing_data$prc_value >= 20),]
 rownames(high_rate20)
 
 # Which columns have less than 50% missing data?
 
-low_rate <- missing_data[which(missing_data$prc_value < 50), ]
+low_rate <- missing_data[which(missing_data$prc_value < 50),]
 rownames(low_rate)
 
 # ====== 3 Manipulate the dataset ========================================================================
@@ -109,13 +110,17 @@ data <-
 data <-
   data %>% mutate(weight_gain = as.numeric(Discharged_Weight - Weight__kg_1))
 
-## Calculate of rate of weight gain 
+## Calculate of rate of weight gain
 #Rate of weight gain (gm/kg/day)=(( Discharged_Weight - Weight__kg_1) * 1000) / ((Outcome_Date - Date1) * Weight__kg_1)
 
 data <-
-  data %>% mutate(rate_weight_gain = (weight_gain* 1000) / hospital_stay* Weight__kg_1) # (gm/kg/day)
+  data %>% mutate(rate_weight_gain = (weight_gain * 1000) / hospital_stay * Weight__kg_1) # (gm/kg/day)
 
-# ====== 4 Growth dataset========================================================================
+## Convert character variables to numeric ("Yes" = 1, "No" = 0)
+
+data <-
+  
+  # ====== 4 Growth dataset========================================================================
 
 # Let's keep only the anthropometric measurements and oedema that might have effect on weight for now
 
@@ -184,8 +189,8 @@ na_freq <-
     stringsAsFactors = F
   )
 for (i in 1:ncol(growth_data)) {
-  na_freq[i, ] <-
-    c(col = colnames(growth_data[i]), freq = as.numeric(sum(!is.na(growth_data[, i]))))
+  na_freq[i,] <-
+    c(col = colnames(growth_data[i]), freq = as.numeric(sum(is.na(growth_data[, i]))))
   #print(paste0(colnames(growth_data[i]), ": ", sum(!is.na(growth_data[,i]))))
 }
 
@@ -193,7 +198,7 @@ na_freq$freq <- as.numeric(na_freq$freq)
 
 # Visualizations
 
-ggbarplot(na_freq[c(12:17), ], x = "col", y = "freq")
+ggbarplot(na_freq[c(12:17),], x = "col", y = "freq")
 
 # ====== 5 Clinical dataset========================================================================
 
@@ -210,10 +215,9 @@ clinical <-
     "Haemoptysis",
     "Vomiting",
     "Haematemesis",
-    "Diarrhoea",
-    "Weight__kg_6",
     "Anorexia",
-    "Rashes"
+    "Rashes",
+    "Odema"
   )
 
 biochemical <-
@@ -221,11 +225,49 @@ biochemical <-
     "Hematocrit_PCV",
     "Diff_Leucocyte_Count",
     "Total_Leucocyte_Count",
-    "Neutrophil"
+    "Neutrophil",
+    "Anion_Gap",
+    "ESR",
+    "Hemoglobin",
+    "Potassium",
+    "Sodium",
+    "Chloride"
   )
 
+clinical_list <- c(clinical, biochemical)
+
+clinical_data <- data[, clinical_list]
+
+## Convert character variables to numeric ("Yes" = 1, "No" = 0)
+
+for (i in 1:ncol(clinical_data)) {
+  if (is.character(clinical_data[[1, i]])) {
+    clinical_data[, i] <-
+      as.numeric(ifelse(clinical_data[, i] == "No", 0, 1))
+  }
+}
+
+## Check for missing data in the clinical dataset
+
+na_freq <-
+  data.frame(
+    col = character(ncol(clinical_data)),
+    freq = numeric(ncol(clinical_data)),
+    stringsAsFactors = F
+  )
+for (i in 1:ncol(clinical_data)) {
+  na_freq[i,] <-
+    c(col = colnames(clinical_data[i]), freq = as.numeric(sum(is.na(clinical_data[, i]))))
+  #print(paste0(colnames(growth_data[i]), ": ", sum(!is.na(growth_data[,i]))))
+}
+
+na_freq$freq <- as.numeric(na_freq$freq)
+
+# Visualizations
+
+ggbarplot(na_freq[c(12:17),], x = "col", y = "freq")
 
 #====== 6 Compute descriptive statistics =================================================
 
-res <- stat.desc(growth_data[,-c(1:11, 30)])
+res <- stat.desc(growth_data[, -c(1:11, 30)])
 round(res, 2)
